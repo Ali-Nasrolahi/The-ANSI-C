b1126f777bf3d4a6525fe2fedb57ed7e443597a9
Ali .N <A.Nasrolahi01@gmail.com>
2022-03-16 10:29:13 +0330

    Chapter01: new files added

---
+++ Chapter01/ex1-19.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAXCHAR 1000
+main()
+{
+    while (gline() > -1)
+        ;
+}
+
+int gline()
+{
+
+    int i, c, limit;
+    char line[MAXCHAR], rev[MAXCHAR];
+    limit = 100;
+    for (i = 0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+        line[i] = c;
+
+    while (i > -1)
+        putchar(line[--i]);
+
+    putchar('\n');
+
+    if (c == '\n')
+        return 0;
+
+    return -1;
+}
