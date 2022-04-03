4efd1af96b91b2eb0e705593621efc6274152375
Ali .N <A.Nasrolahi01@gmail.com>
2022-04-03 17:04:26 +0430

    ex2-1.c added

--- Chapter02/ex2-1.c
+++ Chapter02/ex2-1.c
@@ -2,16 +2,24 @@
 #include <limits.h>
 #include <stdio.h>
 
-void print_bytes_in_dec(const char *type, unsigned long long byte) {
+void print_bytes_in_dec(const char *type, long long byte)
+{
 
-  printf("%llu\n", byte);
-  printf("signed %s : from -%llu to %llu\n", type, (1 << (byte * 8 - 1)),
+  printf("signed %s : from -%ld to %ld\n", type, (1 << (byte * 8 - 1)),
          (1 << (byte * 8 - 1)) - 1);
-  printf("unsigned %s : from 0 to %llu\n", type, (1 << (byte * 8)));
+  printf("unsigned %s : from 0 to %ld\n", type, (1 << (byte * 8)));
 }
-main() {
+main()
+{
   print_bytes_in_dec("char", sizeof(char));
+  print_bytes_in_dec("short", sizeof(short));
+
   print_bytes_in_dec("int", sizeof(int));
-  print_bytes_in_dec("long long", sizeof(long long));
+
+  // Unsigned version has some issues, so we use
+  printf("unsigned %s : from %ld to %ld\n", "int", 0, UINT_MAX);
+
+  // For long we use constants
+  printf("signed %s : from %ld to %ld\n", "long", LONG_MIN, LONG_MAX);
   return;
 }
